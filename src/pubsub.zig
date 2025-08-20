ff643787b344d29adc760633bf5019c9efbc9ff7
elpekenin <elpekenin@elpekenin.dev>
2025-08-20 15:24:37 +0200

    completely type safe

--- src/pubsub.zig
+++ src/pubsub.zig
@@ -1,5 +1,6 @@
-fn Tag(comptime E: type) type {
-    const info = @typeInfo(E);
+/// Get the underlying `enum` type from a tagged union
+fn Tag(comptime Event: type) type {
+    const info = @typeInfo(Event);
 
     if (info == .@"union") {
         if (info.@"union".tag_type) |tag_type| {
@@ -10,62 +11,122 @@ fn Tag(comptime E: type) type {
     @compileError("expected `union (enum)` as input");
 }
 
-fn Payload(comptime E: type, comptime tag: Tag(E)) type {
-    return @FieldType(E, @tagName(tag));
+/// Given an event type (tagged union), get the payload associated
+/// to one of its tags
+fn Payload(comptime Event: type, comptime tag: Tag(Event)) type {
+    return @FieldType(Event, @tagName(tag));
 }
 
-fn CallbackFn(comptime E: type, comptime tag: Tag(E)) type {
-    return *const fn (Payload(E, tag)) void;
+/// Signature of a callback function for a given tag
+fn CallbackFn(comptime Event: type, comptime tag: Tag(Event)) type {
+    return *const fn (Payload(Event, tag)) void;
 }
 
-/// create an event bus for the given tagged union
-pub fn EventBus(comptime E: type) type {
-    const Subscription = struct {
-        tag: Tag(E),
-        /// can't have specific function type here because the specific tag is unknown at this point
-        ///
-        /// this is still safe(ish) because `.subscribe` ensures function is of type `CallbackFn(E, tag)`
-        ///
-        /// if user decides to add an entry manually, that's their problem ^^
-        /// ```zig
-        /// // DONT DO THIS, BREAKAGE INCOMING !!
-        /// try bus.subscriptions.append(bus.allocator, .{
-        ///     .tag = .tag_name,
-        ///     .callback = invalidFunction,
-        /// });
-        /// ```
-        callback: *const anyopaque,
-    };
+/// Helper to get the fields from the underlying enum
+fn eventFields(comptime Event: type) []const Type.EnumField {
+    return @typeInfo(Tag(Event)).@"enum".fields;
+}
 
+/// Create a struct type from the given event type
+/// 
+/// It will have a field for each tag, which will store all callbacks
+/// registered for such tag (in an `ArrayList`)
+fn Subscriptions(comptime Event: type) type {
+    var fields: []const Type.StructField = &.{};
+
+    for (eventFields(Event)) |field| {
+        const tag = @field(Tag(Event), field.name);
+        const T = std.ArrayList(CallbackFn(Event, tag));
+
+        fields = fields ++ &[_]Type.StructField{
+            .{
+                .name = field.name,
+                .type = T,
+                .default_value_ptr = null,
+                .is_comptime = false,
+                .alignment = std.meta.alignment(T),
+            },
+        };
+    }
+
+    return @Type(.{ .@"struct" = .{
+        .layout = .auto,
+        .fields = fields,
+        .decls = &.{},
+        .is_tuple = false,
+    } });
+}
+
+/// Create an event bus type for the given event type
+///
+/// The event type is a tagged union that describes the different
+/// existing events, and the payload associated to each of them.
+///
+/// Example:
+/// ```zig
+/// const EventBus = pubsub.EventBus(union (enum) {
+///     login: UserId,
+///     logout: UserId,
+///     message_sent: struct {
+///         from: UserId,
+///         to: UserId,
+///         text: []cont u8
+///     },
+/// });
+pub fn EventBus(comptime Event: type) type {
     return struct {
         allocator: std.mem.Allocator,
-        subscriptions: std.ArrayList(Subscription),
+        subscriptions: Subscriptions(Event),
 
-        pub fn init(allocator: std.mem.Allocator) Bus {
-            return .{
+        fn callbacksFor(self: *Self, comptime tag: Tag(Event)) *std.ArrayList(CallbackFn(Event, tag)) {
+            return &@field(self.subscriptions, @tagName(tag));
+        }
+
+        /// Initialize an instance of the event bus
+        pub fn create(allocator: std.mem.Allocator) Self {
+            var self: Self = .{
                 .allocator = allocator,
-                .subscriptions = .empty,
+                // SAFETY: initialized below
+                .subscriptions = undefined,
             };
+
+            inline for (eventFields(Event)) |field| {
+                const tag = @field(Tag(Event), field.name);
+                self.callbacksFor(tag).* = .empty;
+            }
+
+            return self;
         }
 
-        pub fn subscribe(bus: *Bus, comptime tag: Tag(E), callback: CallbackFn(E, tag)) !void {
-            try bus.subscriptions.append(bus.allocator, .{
-                .tag = tag,
-                .callback = callback,
-            });
+        /// Register a new callback for an event
+        /// 
+        /// Example:
+        /// ```zig
+        /// fn loginHandler(user: UserId) void {
+        ///     // do something
+        /// }
+        /// 
+        /// try bus.subscribe(.login, loginHandler);
+        /// ```
+        pub fn subscribe(self: *Self, comptime tag: Tag(Event), callback: CallbackFn(Event, tag)) !void {
+            try self.callbacksFor(tag).append(self.allocator, callback);
         }
 
-        pub fn publish(bus: *const Bus, comptime tag: Tag(E), payload: Payload(E, tag)) void {
-            for (bus.subscriptions.items) |subscription| {
-                if (subscription.tag != tag) continue;
-
-                const callback: CallbackFn(E, tag) = @ptrCast(subscription.callback);
+        /// Emit an event
+        ///
+        /// Example:
+        /// ```zig
+        /// bus.publish(.login, user.id);
+        /// ```
+        pub fn publish(self: *Self, comptime tag: Tag(Event), payload: Payload(Event, tag)) void {
+            for (self.callbacksFor(tag).items) |callback| {
                 callback(payload);
             }
         }
 
-        const Bus = @This();
+        const Self = @This();
     };
 }
 
 const std = @import("std");
+const Type = std.builtin.Type;
